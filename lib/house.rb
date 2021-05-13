c8d0a376dfaf8888938317354475ed3d52dc26f0
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-13 11:48:48 -0700

    added rat method to case 9

--- lib/house.rb
+++ lib/house.rb
@@ -21,7 +21,7 @@
         when 9
             "#{beginning} the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{rat} #{malt} #{house}"
         when 10
-            "#{beginning} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
+            "#{beginning} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{rat} #{malt} #{house}"
         when 11
             "#{beginning} the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 12
