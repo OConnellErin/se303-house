f926882fe69d1b28905422e74444c9ce0b2c8ce3
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-13 11:46:13 -0700

    added rat method

--- lib/house.rb
+++ lib/house.rb
@@ -5,27 +5,27 @@
         when 1    
             "#{beginning} #{house}"
         when 2
-            "#{beginning} the malt that lay in #{house}"
+            "#{beginning} #{malt} #{house}"
         when 3
-            "#{beginning} the rat that ate the malt that lay in #{house}"    
+            "#{beginning} the rat that ate #{malt} #{house}"    
         when 4
-            "#{beginning} the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the cat that killed the rat that ate #{malt} #{house}"
         when 5
-            "#{beginning} the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 6
-            "#{beginning} the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 7
-            "#{beginning} the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 8
-            "#{beginning} the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 9
-            "#{beginning} the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 10
-            "#{beginning} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 11
-            "#{beginning} the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         when 12
-            "#{beginning} the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in #{house}"
+            "#{beginning} the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate #{malt} #{house}"
         end        
     end
 
@@ -40,6 +40,11 @@
     def malt
         "the malt that lay in"
     end
+
+    def rat
+        "the rat that ate"
+    end
+
 
     def recite
         1.upto(12).collect { |i| line(i) }.join("\n")
