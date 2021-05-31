da98d0644807c2fd4d5042d5878835d43defcf91
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-30 23:13:37 -0700

    added init for randompirateship

--- lib/house.rb
+++ lib/house.rb
@@ -126,44 +126,11 @@ class RandomHouse < House
     end
 end
 
-class RandomPirateShip < House
+class RandomPirateShip < RandomHouse
 
     def initialize
         @repeat_times = 0
         @beginning = "Thar be"
-    end
-
-    def noun(disregaurd)
-        number = rand(1..11)
-        @repeat_times = @repeat_times + 1
-        if (@repeat_times <= 12)
-            case(number)
-            when 1
-                "malt that lay in"
-            when 2
-                "rat that ate the #{noun(1)}"
-            when 3
-                "cat that killed the #{noun(2)}"
-            when 4
-                "dog that worried the #{noun(3)}"
-            when 5
-                "cow with the crumpled horn that tossed the #{noun(4)}"    
-            when 6
-                "maiden all forlorn that milked the #{noun(5)}"
-            when 7
-                "man all tattered and torn that kissed the #{noun(6)}" 
-            when 8
-                "priest all shaven and shorn that married the #{noun(7)}"
-            when 9
-                "rooster that crowed in the morn that woke the #{noun(8)}"
-            when 10
-                "farmer sowing his corn that kept the #{noun(9)}"
-            when 11
-                "horse and the hound and the horn that belonged to the #{noun(10)}"
-            end
-        end        
-    end
+    end 
     
-    
-
  end
