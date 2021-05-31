68d9bac942263fbd5423ca41d002b6eee039eca5
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-30 22:58:04 -0700

    added random class with random noun function

--- lib/house.rb
+++ lib/house.rb
@@ -81,15 +81,42 @@
 class RandomHouse < House
    
     def initialize
-        num = rand(1,2) 
-        if num == 1
+        num = rand(1,2)
+        case num 
+        when num == 1
             @beginning = "This is"
-        elsif num == 2
+        when num == 2
             @beginning = "Thare be"
         else
             @beginning = "This here is"
         end
     end
     
-    
+    def noun(disregaurd)
+        number = rand(1,11)
+        case(number)
+        when 1
+            "malt that lay in"
+        when 2
+            "rat that ate the #{noun(1)}"
+        when 3
+            "cat that killed the #{noun(2)}"
+        when 4
+            "dog that worried the #{noun(3)}"
+        when 5
+            "cow with the crumpled horn that tossed the #{noun(4)}"    
+        when 6
+            "maiden all forlorn that milked the #{noun(5)}"
+        when 7
+            "man all tattered and torn that kissed the #{noun(6)}" 
+        when 8
+            "priest all shaven and shorn that married the #{noun(7)}"
+        when 9
+            "rooster that crowed in the morn that woke the #{noun(8)}"
+        when 10
+            "farmer sowing his corn that kept the #{noun(9)}"
+        when 11
+            "horse and the hound and the horn that belonged to the #{noun(10)}"
+        end        
+    end
 end
